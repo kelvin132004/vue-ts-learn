aca02eb15a857d5a7c5f902215b1d6c93db006fb
Kelvin Refamonte <kelvin132004@yahoo.com>
2023-03-02 21:21:04 +0800

    typescript vue sample

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="number" v-model="num" />
+  <button @click="addNumber">Add</button>
+  <div>{{ records  }}</div>
+ 
+  <ul v-for="row in records">
+    <li>{{ row * 5 }}</li>
+  </ul>
+
+  <h1><strong>{{ total }}</strong></h1>
+
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+
+const num = ref<number>(0);
+const total = ref<number>(0);
+
+const records = reactive<number[]>([]);
+
+function addNumber() {
+  records.push(num.value);
+  Sum();
+}
+
+function Sum() {
+  total.value = total.value + num.value;
+  return total;
+}
+
+</script>
